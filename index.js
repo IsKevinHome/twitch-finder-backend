a786055bdc0b86d325855c272595f822d68389ea
Kevin Muhlbach <HeadbuttStudios@gmail.com>
2022-06-04 19:50:10 -0700

    .

--- index.js
+++ index.js
@@ -40,7 +40,13 @@ app.get("/search/:channels", async (req, res) => {
     res.send(results);
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log("App is running on port " + port);
 });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`App listening on port ${PORT}`);
+// });
